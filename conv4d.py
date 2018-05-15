e12acc09a7b2607ace45d195fa314178c9b98eb2
Jan Funke <jfunke@iri.upc.edu>
2018-05-15 11:43:46 +0200

    Introduce t-dimension to conv4d interface: (t, z, y, x)

--- conv4d.py
+++ conv4d.py
@@ -20,10 +20,10 @@ def conv4d(
         trainable=True,
         name=None,
         reuse=None):
-    '''Performs a 4D convolution (i.e., including the channel dimension) of a
-    tensor ``(b, c, d, h, w)`` with ``k`` kernels. The output tensor will be of
-    shape ``(b, c'*k, d', h', w')``. ``(c', d', h', w')`` will be smaller than
-    ``(c, d, h, w)`` if a ``valid`` padding was chosen.
+    '''Performs a 4D convolution of the ``(t, z, y, x)`` dimensions of a tensor
+    with shape ``(b, c, l, d, h, w)`` with ``k`` filters. The output tensor
+    will be of shape ``(b, k, l', d', h', w')``. ``(l', d', h', w')`` will be
+    smaller than ``(l, d, h, w)`` if a ``valid`` padding was chosen.
 
     This operator realizes a 4D convolution by performing several 3D
     convolutions. The following example demonstrates how this works for a 2D
@@ -90,13 +90,13 @@ def conv4d(
     '''
 
     # check arguments
-    assert len(input.get_shape().as_list()) == 5, (
-        "Tensor of shape (b, c, d, h, w) expected")
+    assert len(input.get_shape().as_list()) == 6, (
+        "Tensor of shape (b, c, l, d, h, w) expected")
     assert len(kernel_size) == 4, "4D kernel size expected"
     assert strides == (1, 1, 1, 1), (
         "Strides other than 1 not yet implemented")
-    assert data_format == 'channels_last', (
-        "Data format other than 'channels_last' not yet implemented")
+    assert data_format == 'channels_first', (
+        "Data format other than 'channels_first' not yet implemented")
     assert dilation_rate == (1, 1, 1, 1), (
         "Dilation rate other than 1 not yet implemented")
 
@@ -104,44 +104,46 @@ def conv4d(
         name = 'conv4d'
 
     # input, kernel, and output sizes
-    (b, c_i, d_i, h_i, w_i) = tuple(input.get_shape().as_list())
-    (c_k, d_k, h_k, w_k) = kernel_size
+    (b, c_i, l_i, d_i, h_i, w_i) = tuple(input.get_shape().as_list())
+    (l_k, d_k, h_k, w_k) = kernel_size
 
     # output size for 'valid' convolution
     if padding == 'valid':
-        (c_o, d_o, h_o, w_o) = (
-            c_i - c_k + 1,
+        (l_o, d_o, h_o, w_o) = (
+            l_i - l_k + 1,
             d_i - d_k + 1,
             h_i - h_k + 1,
             w_i - w_k + 1
         )
     else:
-        (c_o, d_o, h_o, w_o) = (c_i, d_i, h_i, w_i)
+        (l_o, d_o, h_o, w_o) = (l_i, d_i, h_i, w_i)
 
-    # print("Input shape : ", (b, c_i, d_i, h_i, w_i))
-    # print("Kernel shape: ", (c_k, d_k, h_k, w_k))
-    # print("Output shape: ", (b, c_o*filters, d_o, h_o, w_o))
+    print("Input shape : ", (b, c_i, l_i, d_i, h_i, w_i))
+    print("Kernel shape: ", (l_k, d_k, h_k, w_k))
+    print("Output shape: ", (b, filters, l_o, d_o, h_o, w_o))
 
-    # convolve each kernel channel i with each input channel j
-    channel_results = [ None ]*c_o
-    for i in range(c_k):
+    # output tensors for each 3D frame
+    frame_results = [ None ]*l_o
+
+    # convolve each kernel frame i with each input frame j
+    for i in range(l_k):
 
         # reuse variables of previous 3D convolutions for the same kernel
-        # channel (or if the user indicated to have all variables reused)
+        # frame (or if the user indicated to have all variables reused)
         reuse_kernel = reuse
 
-        for j in range(c_i):
+        for j in range(l_i):
 
-            # add results to this output channel
-            out_channel = j - (i - c_k/2) - (c_i - c_o)/2
-            # print("Conv of kernel %d with input %d goes to output %d"%(i, j, out_channel))
-            if out_channel < 0 or out_channel >= c_o:
-                # print("not a valid output channel, skipping")
+            # add results to this output frame
+            out_frame = j - (i - l_k/2) - (l_i - l_o)/2
+            # print("Conv of kernel %d with input %d goes to output %d"%(i, j, # out_frame))
+            if out_frame < 0 or out_frame >= l_o:
+                # print("not a valid output frame, skipping")
                 continue
 
-            # convolve input channel j with kernel channel i
-            channel_conv3d = tf.layers.conv3d(
-                tf.reshape(input[:,j,:], (b, 1, d_i, h_i, w_i)),
+            # convolve input frame j with kernel frame i
+            frame_conv3d = tf.layers.conv3d(
+                tf.reshape(input[:,:,j,:], (b, c_i, d_i, h_i, w_i)),
                 filters,
                 kernel_size=(d_k, h_k, w_k),
                 padding=padding,
@@ -156,19 +158,21 @@ def conv4d(
                 trainable=trainable,
                 name=name + '_3dchan%d'%i,
                 reuse=reuse_kernel)
+
+            # subsequent frame convolutions should use the same kernel
             reuse_kernel = True
 
-            # print("\tInput :", (b, 1, d_i, h_i, w_i))
+            # print("\tInput :", (b, c_i, d_i, h_i, w_i))
             # print("\tKernel:", (d_k, h_k, w_k))
-            # print("\tOutput:", channel_conv3d.get_shape().as_list())
+            # print("\tOutput:", frame_conv3d.get_shape().as_list())
 
-            if channel_results[out_channel] is None:
-                channel_results[out_channel] = channel_conv3d
+            if frame_results[out_frame] is None:
+                frame_results[out_frame] = frame_conv3d
             else:
-                channel_results[out_channel] += channel_conv3d
+                frame_results[out_frame] += frame_conv3d
 
-    output = tf.concat(channel_results, axis=1)
-    # print("Output: ", output.get_shape().as_list())
+    output = tf.stack(frame_results, axis=2)
+    print("Output: ", output.get_shape().as_list())
 
     if activation:
         output = activation(output)
@@ -179,7 +183,7 @@ if __name__ == "__main__":
 
     import numpy as np
 
-    i = np.round(np.random.random((1, 10, 10, 10, 10))*100)
+    i = np.round(np.random.random((1, 1, 10, 11, 12, 13))*100)
     input = tf.constant(i, dtype=tf.float32)
     bias_init = tf.constant_initializer(0)
 
@@ -187,6 +191,7 @@ if __name__ == "__main__":
         input,
         1,
         (3, 3, 3, 3),
+        data_format='channels_first',
         bias_initializer=bias_init,
         name='conv4d_valid')
 
@@ -202,18 +207,18 @@ if __name__ == "__main__":
         k2 = tf.get_default_graph().get_tensor_by_name(
             'conv4d_valid_3dchan2/kernel:0').eval().flatten()
 
-        print("conv4d at (0, 0, 0, 0): ", o[0,0,0,0,0])
-        i0 = i[0,0,0:3,0:3,0:3].flatten()
-        i1 = i[0,1,0:3,0:3,0:3].flatten()
-        i2 = i[0,2,0:3,0:3,0:3].flatten()
+        print("conv4d at (0, 0, 0, 0): ", o[0,0,0,0,0,0])
+        i0 = i[0,0,0,0:3,0:3,0:3].flatten()
+        i1 = i[0,0,1,0:3,0:3,0:3].flatten()
+        i2 = i[0,0,2,0:3,0:3,0:3].flatten()
 
         compare = (i0*k0 + i1*k1 + i2*k2).sum()
         print("manually computed value at (0, 0, 0, 0): ", compare)
 
-        print("conv4d at (4, 4, 4, 4): ", o[0,4,4,4,4])
-        i0 = i[0,4,4:7,4:7,4:7].flatten()
-        i1 = i[0,5,4:7,4:7,4:7].flatten()
-        i2 = i[0,6,4:7,4:7,4:7].flatten()
+        print("conv4d at (4, 4, 4, 4): ", o[0,0,4,4,4,4])
+        i0 = i[0,0,4,4:7,4:7,4:7].flatten()
+        i1 = i[0,0,5,4:7,4:7,4:7].flatten()
+        i2 = i[0,0,6,4:7,4:7,4:7].flatten()
 
         compare = (i0*k0 + i1*k1 + i2*k2).sum()
         print("manually computed value at (4, 4, 4, 4): ", compare)
@@ -223,6 +228,7 @@ if __name__ == "__main__":
         input,
         1,
         (3, 3, 3, 3),
+        data_format='channels_first',
         padding='same',
         kernel_initializer=tf.constant_initializer(1),
         bias_initializer=bias_init,
@@ -233,14 +239,14 @@ if __name__ == "__main__":
         s.run(tf.global_variables_initializer())
         o = s.run(output)
 
-        print("conv4d at (0, 0, 0, 0): ", o[0,0,0,0,0])
-        i0 = i[0,0:2,0:2,0:2,0:2]
+        print("conv4d at (0, 0, 0, 0): ", o[0,0,0,0,0,0])
+        i0 = i[0,0,0:2,0:2,0:2,0:2]
         print("manually computed value at (0, 0, 0, 0): ", i0.sum())
 
-        print("conv4d at (5, 5, 5, 5): ", o[0,5,5,5,5])
-        i5 = i[0,4:7,4:7,4:7,4:7]
+        print("conv4d at (5, 5, 5, 5): ", o[0,0,5,5,5,5])
+        i5 = i[0,0,4:7,4:7,4:7,4:7]
         print("manually computed value at (5, 5, 5, 5): ", i5.sum())
 
-        print("conv4d at (9, 9, 9, 9): ", o[0,9,9,9,9])
-        i9 = i[0,8:,8:,8:,8:]
+        print("conv4d at (9, 9, 9, 9): ", o[0,0,9,9,9,9])
+        i9 = i[0,0,8:,8:,8:,8:]
         print("manually computed value at (9, 9, 9, 9): ", i9.sum())
